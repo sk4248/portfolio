3d891e9ff549858e91f40ff0be9174988e3764fb
sreekanth <sreekanth@call-em-all.com>
2018-09-12 09:19:37 -0500

    started a budget application in react(2nd react app)

---
+++ src/app.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+
+ReactDom.render(<p>This is a boiler plate</p>, document.getElementById('app'));
+  
